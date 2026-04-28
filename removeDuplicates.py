c61d3248f4a0133fafe00000a2234f7448552913
bettercallmefifi <ferdaouskai@gmail.com>
2026-04-29 00:51:20 +0100

    remove Duplicates

---
+++ removeDuplicates.py
@@ -0,0 +1,7 @@
+def removeDuplicates(self, nums: List[int]) -> int:
+    i = len(nums) - 1
+    while i > 0:
+        if nums[i] == nums[i - 1]:
+                nums.remove(nums[i])
+        i -= 1
+    return len(nums)
